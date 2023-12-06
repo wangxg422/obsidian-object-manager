c09b7e43186811e7c5404a583a296661d3eb6566
wangxg <wangxg422@outlook.com>
2023-12-06 20:07:11 +0800

    使用moment格式化时间

--- utils/time.ts
+++ utils/time.ts
@@ -1,4 +1,5 @@
+import moment from "moment"
+
 export function getTime() {
-    const time = new Date()
-    return `${time.getFullYear()}${time.getMonth() + 1}${time.getDate()}${time.getHours()}${time.getMinutes()}${time.getSeconds()}${time.getMilliseconds()}`
+    return moment().format('YYYYMMDDHHmmssSSS')
 }
